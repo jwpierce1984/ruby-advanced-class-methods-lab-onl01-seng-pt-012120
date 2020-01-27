6cb805cea58f4f49921579c42004a3ade27ba0a3
Justin Pierce <jwpierce1984@gmail.com>
2020-01-27 21:46:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,5 +40,11 @@ class Song
     artist_name = parts[0]
     song_name = parts[1].gsub(".mp3", "")
     
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song.save
+    song
+    
   end
 end
